55ae859b6554cae476dc7bfdbbdc937cf458ac65
Florian <j.flo83@laposte.net>
2026-05-25 09:29:06 +0200

    fix delete index

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import postRoutes from "./user.post";
 import getRoutes from "./user.get";
 import putRoutes from "./user.put";
+import deleteRoutes from "./user.delete";
 
 const router = Router();
 
 router.use(postRoutes);
 router.use(getRoutes);
 router.use(putRoutes);
+router.use(deleteRoutes);
 
 export default router;
